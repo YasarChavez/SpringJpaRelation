21081b43c4cb21f25f1a8dba55f38de95dbe3992
YasarChavez <chavezyasar@gmail.com>
2023-09-08 22:19:12 -0300

    Initial commit

---
+++ src/main/java/com/jpa/entities/Club.java
@@ -0,0 +1,29 @@
+package com.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToOne(targetEntity = Coach.class, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_coach")
+    private Coach coach;
+    @OneToMany(targetEntity = Player.class, fetch = FetchType.LAZY, mappedBy = "club")
+    private List<Player> players;
+
+    @ManyToOne(targetEntity = FootballAssociation.class)
+    @JoinColumn(name = "id_football_association")
+    private FootballAssociation footballAasociation;
+
+}
